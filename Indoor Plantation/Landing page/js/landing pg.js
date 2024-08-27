d3f760405558176633692f272954c9ce036a057e
Aman Gupta <ag9797191@gmail.com>
2024-08-27 12:13:08 +0530

    add

--- Indoor Plantation/Landing page/js/landing pg.js
+++ Indoor Plantation/Landing page/js/landing pg.js
@@ -13,6 +13,44 @@ hiddenElements.forEach((el) =>
     observer.observe(el)
 )
 
+let endingDAte = "10 september 2024 7:00 PM";
+let timerValue = document.querySelectorAll("#timerValue");
+function Timer() {
+    let ending = new Date(endingDAte);
+    let now = new Date();
+    let difference = (ending - now) / 1000;
+    timerValue[0].innerHTML = Math.floor(difference / 3600 / 24)
+    timerValue[1].innerHTML = Math.floor(difference / 3600) % 24
+    timerValue[2].innerHTML = Math.floor(difference / 60) % 60
+    timerValue[3].innerHTML = Math.floor(difference) % 60
+}
+Timer()
+setInterval(
+    () => {
+        Timer()
+    },
+    1000
+)
 
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 
+});
 
+// const button = document.getElementById('myButton');
+// const loader = document.getElementById('loader');
+
+// button.addEventListener('click', () => {
+//   loader.style.display = 'block';
+//   // simulate a delay or an AJAX request
+//   setTimeout(() => {
+//     loader.style.display = 'none';
+//     // do something after the loader is hidden
+//   }, 2000);
+// });
